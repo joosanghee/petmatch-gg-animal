137862310f770b8c7f4c002503827b3970e3e23e
UnknownError <89399422+unerror01@users.noreply.github.com>
2025-12-13 14:19:30 +0900

    data_preprocessing-3

--- data/processed/data_preprocess.sql
+++ data/processed/data_preprocess.sql
@@ -27,7 +27,7 @@ ORDER BY addr ASC;
 CREATE TABLE if not EXISTS protection AS
 SELECT 
     발견장소 AS findplace,
-    소재지지번주소 AS addr,
+    보호소지번주소 AS addr,
 	공고고유번호 AS fid,
 	공고시작일자 as startdate,
 	공고종료일자 as enddate,
@@ -37,8 +37,10 @@ SELECT
 	체중 as weight,
 	성별 as sex,
 	중성화여부 as neutering,
-	특징 as 
-	
-FROM animal_pharmacy_status
+	특징 as feature,
+	보호소명 as proname,
+	보호소전화번호 as pronum,
+	이미지경로 as photolink
+FROM stray_animal_protection_status
 WHERE 상태 = '보호중'
 ORDER BY addr ASC;
